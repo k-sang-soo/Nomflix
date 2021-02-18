952895747bffb18776f328c02a81db0bf4432898
k-sang-soo <wsckm1@gmail.com>
2021-02-18 17:42:29 +0900

    #2.2 version

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default () => (
+  <header>
+    <ul>
+      <li>
+        <a href="/">Movies</a>
+      </li>
+
+      <li>
+        <a href="/tv">tv</a>
+      </li>
+
+      <li>
+        <a href="/search">Search</a>
+      </li>
+    </ul>
+  </header>
+);
